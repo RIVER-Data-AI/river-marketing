c62ca4cad533e5925ddc1a5ce8ab6c9973295039
Quinton Harris <quinton.harris@carbonfive.com>
2021-04-29 16:42:47 -0700

    Unstyled MVP.

---
+++ assets/js/river.js
@@ -0,0 +1,66 @@
+const river = (() => {
+  const scrollOptions = {
+    behavior: "smooth",
+  };
+
+  const currentlyVisibleSection = () => {
+    const sections = document.getElementsByClassName("section");
+    for (const section of sections) {
+      const sectionRect = section.getBoundingClientRect();
+      const midY = (sectionRect.top + sectionRect.bottom) / 2;
+      if (midY > window.scrollY && midY < window.scrollY + window.innerHeight) {
+        return section;
+      }
+    }
+  };
+
+  const previousSection = () => {
+    const section = currentlyVisibleSection();
+    const targetSection = section?.previousElementSibling;
+    if (!targetSection) {
+      return;
+    }
+
+    targetSection.scrollIntoView(scrollOptions);
+  };
+
+  const nextSection = () => {
+    const section = currentlyVisibleSection();
+    const targetSection = section?.nextElementSibling;
+    if (!targetSection) {
+      return;
+    }
+
+    // Safari apparently has an issue with smooth scrolling; let's punt that for now.
+    // https://stackoverflow.com/questions/51229742/javascript-window-scroll-behavior-smooth-not-working-in-safari
+    section.nextElementSibling.scrollIntoView(scrollOptions);
+  };
+
+  document.addEventListener("keyup", (event) => {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+
+    switch (event.key) {
+      case "Down":
+      case "ArrowDown":
+      case "PageDown":
+        nextSection();
+        break;
+
+      case "Up":
+      case "ArrowUp":
+      case "PageUp":
+        previousSection();
+        break;
+
+      default:
+        return;
+    }
+  });
+
+  return {
+    previousSection,
+    nextSection,
+  };
+})();
